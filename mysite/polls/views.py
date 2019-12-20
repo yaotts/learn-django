bfdfa2459abd2ce4022356693215845d5f461274
yaotts <yaotts@hotmail.com>
2019-12-20 19:29:59 +0800

    move files from pi to github

---
+++ mysite/polls/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render,get_object_or_404
+from polls.models import Question,Choice
+from django.urls import reverse
+from django.views import generic
+
+from django.http import HttpResponse,HttpResponseRedirect
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        return Question.objects.order_by("pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name='polls/results.html'
+
+def vote(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,'polls:details.html',{'question':question,'error_message':"Please select a choice."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
