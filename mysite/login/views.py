c54685391a82e520784473d8ba0dab878e9a33d1
yaotts <yaotts@hotmail.com>
2020-01-21 23:27:20 +0800

    complate register page and handle the problem of cleartext password

--- mysite/login/views.py
+++ mysite/login/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
 from . import models
+import hashlib
 
 # Create your views here.
 def home(request):
@@ -9,6 +10,13 @@ def home(request):
         return render(request,'home_login.html')
     else:
         return render(request,'home_logout.html')
+
+def hash_code(p,salt='xixi'):
+    h = hashlib.sha256()
+    p += salt
+    h.update(p.encode('utf-8'))   #如果不加'utf-8',在输入密码时，用的又是中文输入法，会触发‘CSRF验证失败的错误’
+    return h.hexdigest()
+
 
 def login(request):
     if request.session.get('is_login',None):
@@ -23,7 +31,7 @@ def login(request):
                 message = '用户名不存在！'
                 return render(request, 'login/login.html',{'message': message})
             
-            if user.password == password:
+            if user.password == hash_code(password):
                 request.session['is_login'] = True
                 request.session['user_id'] = user.id
                 request.session['user_name'] = user.name
@@ -40,7 +48,36 @@ def login(request):
 
 
 def register(request):
-    pass
+    if request.session.get('is_login',None):
+        return HttpResponseRedirect(reverse('home'))
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        email = request.POST.get('email')
+
+        same_name = models.User.objects.filter(name=username)
+        same_email = models.User.objects.filter(email=email)
+
+        if same_name:
+            message = '用户名已被注册'
+            return render(request, 'login/register.html', {'message': message})
+        else:
+            if password1 != password2:
+                message = '两次输入的密码不同'
+                return render(request,'login/register.html',{'message':message})
+
+            if same_email:
+                message = '邮箱已被注册'
+                return render(request, 'login/register.html', {'message': message})
+            
+            new_user = models.User()
+            new_user.name = username
+            new_user.password = hash_code(password1)
+            new_user.email = email
+            new_user.save()
+            print('注册成功')
+            return HttpResponseRedirect(reverse('login:login'))
     return render(request,'login/register.html')
 
 def logout(request):
